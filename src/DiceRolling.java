2e2f69d1bc21d20fb253323d246253315d90c105
Davian Affognon <affogdav000@warren.k12.in.us>
2024-09-23 11:18:28 -0400

    ok

--- src/DiceRolling.java
+++ src/DiceRolling.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class DiceRolling {
+import java.util.Random;
+
+class App {
+    public static void main(String[] args) {
+
+        rollSixDice();
+        rollSixDice();
+        rollSixDice();
+        rollSixDice();
+        rollSixDice();
+    }
+
+    private static void rollSixDice() {
+        Random rand = new Random();
+        System.out.print("6 dices roll: ");
+        for (int i = 0; i < 6; i++) {
+            int roll = rand.nextInt(6) + 1;
+            System.out.print("Roll #" + (i + 1) + ": " + roll + " ");
+        }
+        System.out.println();
+    }
 }
